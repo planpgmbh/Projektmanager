6c135313263b6d7b28b54022b665c1e842dcf693
planp <36702309+planpgmbh@users.noreply.github.com>
2025-08-12 21:32:18 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { setupAPI } from './api';
 
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    {
+      name: 'setup-express',
+      configureServer(server) {
+        setupAPI(server);
+      }
+    }
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
   },
   build: {
     outDir: 'dist',
@@ -32,5 +29,9 @@ export default defineConfig({
         }
       }
     }
+  },
+  base: '/',
+  server: {
+    port: 5173
   }
 });
